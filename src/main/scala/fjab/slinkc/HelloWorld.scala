1ce6572cc281afb0675356ad0aa83fd1dbc45d8f
Francisco Alvarez <fjab76@gmail.com>
2016-09-18 18:15:39 +0100

    Tail recursive implementation

---
+++ src/main/scala/fjab/slinkc/HelloWorld.scala
@@ -0,0 +1,13 @@
+package fjab.slinkc
+
+/**
+ * Created by franciscoalvarez on 08/09/2016.
+ */
+object HelloWorld {
+
+  def main(args: Array[String]) {
+    print("hello world")
+  }
+
+  def printHelloWorld() = print("hello world")
+}
---
+++ src/main/scala/fjab/slinkc/MongoDb.scala
@@ -0,0 +1,19 @@
+package fjab.slinkc
+
+import org.mongodb.scala.{Document, MongoCollection, MongoDatabase, MongoClient}
+
+/**
+ * Created by franciscoalvarez on 12/09/2016.
+ */
+object MongoDb {
+
+//  // To directly connect to the default server localhost on port 27017
+//  val mongoClient: MongoClient = MongoClient()
+//
+//  val database: MongoDatabase = mongoClient.getDatabase("search")
+//
+//  val collection: MongoCollection[Document] = database.getCollection("quotation");
+
+
+
+}
